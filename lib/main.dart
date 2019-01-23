e5513130b8f168e7a8f487a11fe405723b5803f4
Adrian Stabiszewski <as@grundid.de>
2019-01-23 09:29:28 +0100

    display DB info during startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:blaulichtplaner_app/launch_screen_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+Future main() async {
   // debugPaintSizeEnabled = true;
   Firestore firestore = Firestore.instance;
+  FirebaseOptions options = await firestore.app.options;
+  print("Using database: ${options.databaseURL}");
   firestore.settings(timestampsInSnapshotsEnabled: true);
   initializeDateFormatting();
   runApp(ShiftplanApp());

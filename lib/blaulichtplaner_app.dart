b3ab6e836729cbdccb855bc64a8a4db1adecd54d
Adrian Stabiszewski <as@grundid.de>
2019-02-18 09:53:52 +0100

    ios version of notifications

--- lib/blaulichtplaner_app.dart
+++ lib/blaulichtplaner_app.dart
@@ -1,16 +1,14 @@
 import 'package:blaulichtplaner_app/assignment/assignment_tab.dart';
 import 'package:blaulichtplaner_app/authentication.dart';
-import 'package:blaulichtplaner_app/firestore/firestore_flutter.dart';
 import 'package:blaulichtplaner_app/invitation/invitation_view.dart';
 import 'package:blaulichtplaner_app/shift_vote/shift_votes_tab.dart';
 import 'package:blaulichtplaner_app/shiftplan/shiftplan_overview_tab.dart';
-import 'package:blaulichtplaner_app/utils/utils.dart';
 import 'package:blaulichtplaner_app/widgets/drawer.dart';
+import 'package:blaulichtplaner_app/widgets/notification_view.dart';
 import 'package:blaulichtplaner_lib/blaulichtplaner.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-//import 'package:blaulichtplaner_app/utils/notifications.dart';
+import 'package:blaulichtplaner_app/utils/notifications.dart';
 
 class BlaulichtplanerApp extends StatefulWidget {
   final Function logoutCallback;
@@ -23,9 +21,8 @@ class BlaulichtplanerApp extends StatefulWidget {
   }
 }
 
-class BlaulichtPlanerAppState  extends State<BlaulichtplanerApp>  { //with LocalNotifications
-  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-
+class BlaulichtPlanerAppState extends State<BlaulichtplanerApp>
+    with Notifications {
   int selectedTab = 0;
   BlpUser user;
 
@@ -33,91 +30,11 @@ class BlaulichtPlanerAppState  extends State<BlaulichtplanerApp>  { //with Local
   void initState() {
     super.initState();
     user = UserManager.instance.user;
-   // _initMessaging();
-   // initLocalNotification();
-    //_firebaseMessaging.requestNotificationPermissions();
-  }
-
-
-  @override
-  void didUpdateWidget(BlaulichtplanerApp oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    //_initMessaging();
-  }
-
-
-  void _initMessaging() async {
-    print("init messaging");
-
-    Firestore firestore = FirestoreImpl.instance;
-    String token = await _firebaseMessaging.getToken();
-    print(' gotten $token');
-
-    await _updateTokenIfNecessary(firestore, token);
-
-    _firebaseMessaging.configure(onMessage: (Map<String, dynamic> message) {
-      try {
-        print("onMessage $message");
-        Map<String, String> data = message.containsKey("data")
-            ? Map.castFrom(message['data'])
-            : Map.castFrom(message);
-     //   showNotification(SimpleNotification(message["aps"]["alert"]["title"], message["aps"]["alert"]["body"],
-     //    null, null));
-      } catch (e) {
-        print(e);
-      }
-    }, onLaunch: (Map<String, dynamic> message) {
-      print("OnLaunch: $message");
-      try {
-        Map<String, String> data = message.containsKey("data")
-            ? Map.castFrom(message['data'])
-            : Map.castFrom(message);
-        //_showNotification(NotificationsHelper(data).convertData());
-      } catch (e) {
-        print(e);
-      }
-
-
-    }, onResume: (Map<String, dynamic> message) {
-      print("OnResume: $message");
-     // Navigator.of(context).push(MaterialPageRoute(builder: (context) => Preferences()));
-      try {
-        Map<String, String> data = message.containsKey("data")
-            ? Map.castFrom(message['data'])
-            : Map.castFrom(message);
-        //_showNotification(NotificationsHelper(data).convertData());
-      } catch (e) {
-        print(e);
-      }
+    initNotifications(user.userRef, (String payload) {
+      print("notification selected [$payload]");
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => NotificationView()));
     });
-
-    _firebaseMessaging.onTokenRefresh.listen((String token) {
-      _updateTokenIfNecessary(firestore, token);
-    });
-  }
-
-  Future _updateTokenIfNecessary(Firestore firestore, String token) async {
-    CollectionReference collection =
-        firestore.collection('users').document(user.uid).collection('tokens');
-
-    QuerySnapshot querySnapshot = await collection.getDocuments();
-
-    bool tokenExits = querySnapshot.documents.firstWhere(
-            (snapshot) => snapshot.data['token'] == token,
-            orElse: () => null) !=
-        null;
-
-    if (!tokenExits) {
-      DeviceInfo deviceInfo = await getDeviceInfo();
-
-      collection.add({
-        'token': token,
-        'created': DateTime.now(),
-        'model': deviceInfo.model,
-        'version': deviceInfo.version,
-        'name': deviceInfo.name
-      });
-    }
   }
 
   Widget _createWidget(
--- lib/utils/notifications.dart
+++ lib/utils/notifications.dart
@@ -1,6 +1,12 @@
 import 'dart:convert';
 
-//import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:blaulichtplaner_app/firestore/firestore_flutter.dart';
+import 'package:blaulichtplaner_app/utils/utils.dart';
+import 'package:blaulichtplaner_app/widgets/notification_view.dart';
+import 'package:blaulichtplaner_lib/blaulichtplaner.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'package:intl/intl.dart';
 
@@ -65,28 +71,104 @@ class NotificationsHelper {
   }
 }
 
-/*
-mixin LocalNotifications {
+typedef void NotificationSelected(String payload);
+
+mixin Notifications {
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  NotificationSelected notificationSelected;
 
-  initLocalNotification() {
+  void initNotifications(
+      DocumentReference userRef, NotificationSelected notificationSelected) {
+    this.notificationSelected = notificationSelected;
+    _initLocalNotifications(notificationSelected);
+    _initFirebaseMessaging(userRef);
+    _firebaseMessaging.requestNotificationPermissions();
+  }
+
+  _initLocalNotifications(NotificationSelected notificationSelected) {
     var initializationSettingsAndroid =
         new AndroidInitializationSettings('app_icon');
     var initializationSettingsIOS = new IOSInitializationSettings(
-        onDidReceiveLocalNotification: onDidRecieveLocalNotification);
+        onDidReceiveLocalNotification:
+            (int id, String title, String body, String payload) {
+      notificationSelected(payload);
+    });
     var initializationSettings = new InitializationSettings(
         initializationSettingsAndroid, initializationSettingsIOS);
     flutterLocalNotificationsPlugin.initialize(initializationSettings,
-        onSelectNotification: onSelectNotification);
+        onSelectNotification: (String payload) {
+      notificationSelected(payload);
+    });
   }
 
-  Future onSelectNotification(String payload) async {
-    print("onSelectNotification: $payload");
-    
+  void _initFirebaseMessaging(DocumentReference userRef) async {
+    print("init messaging");
+    String token = await _firebaseMessaging.getToken();
+    print('gotten $token');
+
+    await _updateTokenIfNecessary(userRef, token);
+
+    _firebaseMessaging.configure(onMessage: (Map<String, dynamic> message) {
+      try {
+        print("onMessage $message");
+        if (message.containsKey("aps")) {
+          _showNotification(SimpleNotification(message["aps"]["alert"]["title"],
+              message["aps"]["alert"]["body"], null, null));
+        }
+      } catch (e) {
+        print(e);
+      }
+    }, onLaunch: (Map<String, dynamic> message) {
+      print("OnLaunch: $message");
+      try {
+        notificationSelected(null);
+      } catch (e) {
+        print(e);
+      }
+    }, onResume: (Map<String, dynamic> message) {
+      print("OnResume: $message");
+      try {
+        Map<String, String> data = message.containsKey("data")
+            ? Map.castFrom(message['data'])
+            : Map.castFrom(message);
+        //_showNotification(NotificationsHelper(data).convertData());
+      } catch (e) {
+        print(e);
+      }
+    });
+
+    _firebaseMessaging.onTokenRefresh.listen((String token) {
+      _updateTokenIfNecessary(userRef, token);
+    });
   }
 
-    void showNotification(SimpleNotification notification) async {
+  Future _updateTokenIfNecessary(
+      DocumentReference userRef, String token) async {
+    CollectionReference collection = userRef.collection('tokens');
+
+    QuerySnapshot querySnapshot = await collection.getDocuments();
+
+    bool tokenExits = querySnapshot.documents.firstWhere(
+            (snapshot) => snapshot.data['token'] == token,
+            orElse: () => null) !=
+        null;
+
+    if (!tokenExits) {
+      DeviceInfo deviceInfo = await getDeviceInfo();
+
+      collection.add({
+        'token': token,
+        'created': DateTime.now(),
+        'model': deviceInfo.model,
+        'version': deviceInfo.version,
+        'name': deviceInfo.name
+      });
+    }
+  }
+
+  void _showNotification(SimpleNotification notification) async {
     print('show notification ${notification.title} ${notification.body}');
 
     try {
@@ -124,14 +206,4 @@ mixin LocalNotifications {
       print('notifications error: $e');
     }
   }
-
-
-  Future onDidRecieveLocalNotification(
-      int id, String title, String body, String payload) async {
-          ("location notification received: $id, $payload");
-        
-  }
-
-
-
-}*/
+}

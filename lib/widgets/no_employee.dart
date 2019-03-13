648a8686b4c981bcfb841ae6f98fc80b66fca83e
Adrian Stabiszewski <as@grundid.de>
2019-03-13 16:06:35 +0100

    wording improved

--- lib/widgets/no_employee.dart
+++ lib/widgets/no_employee.dart
@@ -3,12 +3,18 @@ import 'package:flutter/material.dart';
 class NoEmployee extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          Text("Sie sind noch an keinem Standort als Mitarbeiter registriert.")
-        ],
-        mainAxisAlignment: MainAxisAlignment.center,
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Sie sind noch an keinem Standort als Mitarbeiter registriert.\n\nBitte kontaktieren Sie Ihren Standort-Manager und lassen Sie sich als Mitarbeiter einladen.",
+              textAlign: TextAlign.center,
+            )
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
       ),
     );
   }
